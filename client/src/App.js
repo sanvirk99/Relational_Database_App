d5b3c1c9414f417246f7123110d9863b233115cd
sanvirk99 <sanseerat99@gmail.com>
2022-05-18 18:31:02 -0700

    client and server dev seperate

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+//jsx mix of html and js can return htmll
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
